a6b53c4079e070439b0017cb7a5244602512a994
Esteban Santos <santosesteban09@gmail.com>
2021-06-08 18:43:49 -0500

    I was able to add Textarea to the page, replaced each input for textarea and able to use the style sheet. Updated the readme.md with screenshot and working on showing the link.

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ Work Day Scheduler is a simple that allows the user to input events per hour tim
 
 As the day progresses, the colors of the time blocks will change color. Grey signifies the hour timeblock has passed. Green means the event is in the future. Red references that the event is currently in progress. Functionality of the web app can be seen below.
 
+## Project Process
+I began this project with some starter code. I began by choosing what looked most similar to the mock-up, mainly the table from bootstrap. From there I began working on the javascript, but attempting and learning to use JQUERY methods and punctuation. I was most used to vanilla javascript, so I when I began looking for ideas on how save and load items to storage, I used an old example To-DO list from previous week. It was using javascript though so I needed to try to change it to JQUERY. I spent many hours trying to make it work, but was stuck. I began looking across the internet was able to find a possible version of what I needed. It was the .each method. This greatly simplified the process and even the code. It was meant I did not really need a for loop or extra variables. Through the use of the .each method, I was able to have the code look at each timeblock and save what was written, later load what previously saved and then finally checking time using moment(). This saved me a lot of time. 
+
+I was able to get the code to work, so next I went to look at the style, which had been pre-provided for the exercise. I just had to tie to the correct sections. Most were pretty self-explanatory. The hardest one to tie to the style sheet was textarea. I replaced all '<input>' with it and it worked for the most part. It does not look exactly like the mock-up, but it does look close enough, but more importantly, it meets the functionality of the Acceptance criteria.
 
 ## Project Link
 [PLACEHOLDER]
 
 ## Project Screenshot/Functionality
-PICTURE
+![User clicks on the slots on the color-coded calendar, inputs their events.](./assets/images/WorkDayScheduler.gif)
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,44 +8,44 @@ console.log('This is the time in 24h format: ' + nowTime);
 nowDate.text('Today is ' + currentDate);
 
 // This section is for the load from console.
-function loadEvents(){
-  $('input').each(function(){
+function loadEvents() {
+  $('textarea').each(function () {
     var id = $(this).attr('id');
     var event = localStorage.getItem(id);
     $(this).val(event);
-  });    
+  });
 };
 
 //  Save using JQUERY .each method
-savebtn.on('click', function(){
+savebtn.on('click', function () {
 
-  $('input[type="text"]').each(function(){    
-      var id = $(this).attr('id');
-      var value = $(this).val();
-     localStorage.setItem(id, value);
+  $('textarea[type="text"]').each(function () {
+    var id = $(this).attr('id');
+    var value = $(this).val();
+    localStorage.setItem(id, value);
 
-  });   
+  });
 });
 // This section will be to check timeblocks and change their color depending on what current time it is with respect to the timeblock.
 // Present = red
 // Past = grey
 // Future = green
 console.log(12 === 12);
-function checkEvents(){
-$('input').each(function(){
-  var id = $(this).attr('id');
-  var integer = parseInt(id);
-  console.log('This is the current time block: ' + integer);
-  console.log(integer === nowTime);
-  if (integer == nowTime){
-    $(this).addClass('present');
-    console.log('this is present');
-  }else if (integer > nowTime){
-    $(this).addClass('future');
-  }else if (integer < nowTime){
-    $(this).addClass('past');
-  }
-});
+function checkEvents() {
+  $('textarea').each(function () {
+    var id = $(this).attr('id');
+    var integer = parseInt(id);
+    console.log('This is the current time block: ' + integer);
+    console.log(integer === nowTime);
+    if (integer == nowTime) {
+      $(this).addClass('present');
+      console.log('this is present');
+    } else if (integer > nowTime) {
+      $(this).addClass('future');
+    } else if (integer < nowTime) {
+      $(this).addClass('past');
+    }
+  });
 }
 // This initiates functions for loading of variables from storage.
 // It also initiates the checking of each timeblock and comparing it to the current time. It sets the color accordingly. Web app most be reloaded for colors to change according to the time.
